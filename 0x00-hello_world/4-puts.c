83bc61c8d5fea56eb53e063a2512d88e0ffee4a9
Coll76 <collins.kubu@gmail.com>
2023-07-16 12:19:56 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- * main - hello c
- * return: 0 (success)
+ * main - Programm that prints a line with puts function
+ * return: 0(success)
  */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio>
+/**
+ * main - program that print a line using printf
+ * Return:0 (Success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+}
