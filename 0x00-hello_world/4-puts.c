dbf643102b1e86bf94d9cd54b5ae51bd7659b7ab
Coll76 <collins.kubu@gmail.com>
2023-07-16 11:05:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main-program to use put s function
+ * return:0(success)
+ */
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
