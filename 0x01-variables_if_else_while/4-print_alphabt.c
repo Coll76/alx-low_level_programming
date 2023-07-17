cebf4d891bae15d789c93c19cc2c3cd3f8a13c3c
Coll76 <collins.kubu@gmail.com>
2023-07-17 15:58:36 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<ctype.h>
+/**
+ * main - Return point
+ * description-' Print lowercase apart q and e'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lowerCase = 'a';
+
+	while (lowerCase <= 'z')
+	{
+	if (lowerCase  == 'q' || lowerCase == 'e')
+	{
+	continue;
+	}
+	putchar(lowerCase);
+	lowerCase++;
+	}
+	putchar('\n');
+	return (0);
+}
