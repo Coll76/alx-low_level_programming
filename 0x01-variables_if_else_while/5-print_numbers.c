0147db1d2658b6ca0b486bd18503ab9163714e6b
Coll76 <collins.kubu@gmail.com>
2023-07-17 16:27:13 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Return point
+ * description - 'Print numbers of base ten'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+}
