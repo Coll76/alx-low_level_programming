194dff670e8a2d26212ce3d5b28bcbf1bacee9fd
Coll76 <collins.kubu@gmail.com>
2023-07-18 13:55:27 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 'print alphabets 10 times'
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int j = 0;
+
+	while (j <= 10)
+	{
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+	_putchar('\n');
+	}
+}
