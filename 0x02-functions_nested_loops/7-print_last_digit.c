862d7aa74258e4c541761deb4a2207cbb2a84a3a
Coll76 <collins.kubu@gmail.com>
2023-07-18 16:56:14 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - 'Function that prints the last digit'
+ * @c: int to be checked
+ * Return: Value of last digit or 0
+ */
+int print_last_digit(int c)
+{
+	int ld = c % 10;
+
+	if (ld < 0 && ld > 0)
+	{
+	return (ld);
+	}
+	return (0);
+}
