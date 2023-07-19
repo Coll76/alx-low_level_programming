4f2cacdd001d68c64a1d02c4fb0533da4276de8c
Coll76 <collins.kubu@gmail.com>
2023-07-19 15:15:53 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,30 @@
 #include "main.h"
 /**
- * jack_bauer - 'prints every minute of the day from 00:00 to 23:59
+ * jack_bauer - 'print from 00:00 to 23:59'
  */
 void jack_bauer(void)
 {
-	int i, j, k;
+	int a, b, c, d;
 
-	for (i = 0; j < 10; j++)
+	for (a = 0; a <= 2; a++)
 	{
-	for (j = 0; j < 10; j++)
+	for (b = 0; b <= 9; b++)
 	{
-	k = j * i;
-	if (j == 10)
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+	for (c = 0; c <= 5; c++)
 	{
-	_putchar(k + '0');
-	}
-	if (k < 10 && j != 0)
+	for (d = 0; d <= 9; d++)
 	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(k + '0');
-	}
-	else if (k >= 10)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
-	}
-	}
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
 	_putchar('\n');
 	}
+	}
+}
+}
+}
 }
