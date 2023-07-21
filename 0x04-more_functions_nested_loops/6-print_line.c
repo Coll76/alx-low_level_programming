4cc54d55931bf56305f9af987891044cd5b0779a
Coll76 <collins.kubu@gmail.com>
2023-07-20 17:17:46 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - 'function to draw a straight line'
+ * @n: number to be checked
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
