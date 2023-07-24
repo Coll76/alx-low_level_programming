3a573b5087b2b2c478d66402c251554497d0bd79
Coll76 <collins.kubu@gmail.com>
2023-07-24 13:47:28 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_half - 'prints half of a string'
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int strlen(char *s)
+	{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+	longi++;
+	s++;
+	}
+	return (longi);
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - 'function to print n elements'
+ * @a: first value
+ * @n: second value
+ */
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+	printf("%d", a[t]);
+	if (t != n - 1)
+	printf(",");
+	}
+	printf("\n");
+}
