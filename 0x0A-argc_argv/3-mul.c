cbc9b88c34394e37c7b16d81c91d92fa12d2e83b
Coll76 <collins.kubu@gmail.com>
2023-10-01 07:13:42 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply numbers
+ * @argc:argument count
+ * @argv:argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int multiply = 1;
+
+	if (argc == 1)
+	{
+	printf("Error\n");
+	exit(1);
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	multiply = multiply * atoi(argv[i]);
+	printf("%d\n", multiply);
+	}
+	return (0);
+}
