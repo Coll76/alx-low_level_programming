155d6715d83c1709d211c08fb5059afd2c7323da
Coll76 <collins.kubu@gmail.com>
2023-11-02 15:40:27 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size == 0)
 	{
-	return (ptr);
+	free(ptr);
 	return (NULL);
 	}
 	else if (new_size == old_size)
