3fd6c45c4934e8e5fdfadebb55213295b6adfe74
Coll76 <collins.kubu@gmail.com>
2023-11-02 13:39:54 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: array elements
+ * @size: size of array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *ptr = (char *)malloc(size);
+
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+		return (ptr);
+		}
+	}
+}
