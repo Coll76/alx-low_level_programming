cad81dca9356ae2201879003187f1dcaa52a470e
Coll76 <collins.kubu@gmail.com>
2024-02-03 11:09:18 -0800

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,54 @@
 #include "hash_tables.h"
 /**
- * create_item - function that creates item
- * @key: key for the item
- * @value: value assigned to key
- * Return: item created
- */
-hash_node_t *create_item(const char *key, const char *value)
-{
-	hash_node_t *item = (hash_node_t *)malloc(sizeof(hash_node_t));
-
-	item->key = (char *)malloc(strlen(key) + 1);
-	item->value = (char *)malloc(strlen(value) + 1);
-
-	strcpy(item->key, key);
-	strcpy(item->value, value);
-
-	return (item);
-}
-/**
- * hash_djb2 - used to create a hash code/index for the key
+ * hash_djb2 - gives the hashcode/index for the key
  * @str: key
- * Return: index
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str);
 /**
- * hash_table_set - adds an element to the hash table.
- *@ht: hash table you want to add or update the key/value to
- *@key: the key
- *@value: value associated with the key
- *Return: 1 if it succeeded, 0 otherwise
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: hash table you want to add or update the key/value to
+ * @key: key
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	/*creates the item*/
-	hash_node_t *item = create_item(key, value);
-	/*assigns an index to the key*/
-	unsigned long int index = hash_djb2((const unsigned char *)key);
-	/*check if key is empty*/
-	hash_node_t *current = ht->array[index];
+	hash_node_t *ne_node;
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	index = hash_djb2((const unsigned char *)key);
+	current = ht->array[index];
 
 	if (current == NULL)
 	{
 		return (0);
 	}
-	ht->array[index] = item;
+	if (strcmp(key, current->key) == 0)
+	{
+		free(current->value);
+		current->value = strdup(value);
+		return (1);
+	}
+	current = current->next;
+
+	ne_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+
+	if (ne_node == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+	ne_node->key = strdup(key);
+	ne_node->value = strdup(value);
+	ne_node->next = ht->array[index];
+	ht->array[index] = ne_node;
+	}
 	return (1);
 }
