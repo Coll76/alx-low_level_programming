c0e253a97b2a06c684f02bbd49599b5204b0cdf8
Coll76 <collins.kubu@gmail.com>
2024-05-03 05:16:26 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output.
  *@letters: number of letters it should read and print
  *@filename: name of the file
  *Return: actual number of letters it could read and prin
@@ -15,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
+	/*file conains file descriptor returned by open()*/
 	file = open(filename, O_RDONLY);
 
 	if (file == -1)
